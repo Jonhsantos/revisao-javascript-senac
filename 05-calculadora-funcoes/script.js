d92f557ad9fc1569c667e377185a5f4b922545b2
Jonhsantos <jonathanssilvasantos1@gmail.com>
2026-08-11 21:57:12 -0300

    exercicios

--- 05-calculadora-funcoes/script.js
+++ 05-calculadora-funcoes/script.js
@@ -1,29 +1,53 @@
 /*
-  EXERCÍCIO 05 — CALCULADORA UTILIZANDO FUNÇÕES
+EXERCÍCIO 05 — CALCULADORA UTILIZANDO FUNÇÕES
 */
 
 function somar(a, b) {
-  // TODO
+	// TODO
+	return a + b;
 }
 
 function subtrair(a, b) {
-  // TODO
+	// TODO
+	return a - b;
 }
 
 function multiplicar(a, b) {
-  // TODO
+	// TODO
+	return a * b;
 }
 
 function dividir(a, b) {
-  // TODO:
-  // Trate divisão por zero.
+	// TODO:
+	// Trate divisão por zero.
+	if(a === 0 || b === 0){
+		return "Não é possível divisão por Zero"
+	} else{
+		return a / b
+	}
 }
 
 function calcular(a, b, operacao) {
-  // TODO:
-  // Escolha a função correta conforme a operação.
-  // Trate também uma operação desconhecida.
+	// TODO:
+	// Escolha a função correta conforme a operação.
+	// Trate também uma operação desconhecida.
+	if(operacao === "somar"){
+		console.log(somar(a, b))
+	} else if( operacao === "subitrair"){
+		console.log(subtrair(a, b))
+	} else if (operacao === "multiplicar"){
+		console.log(multiplicar(a, b))
+	} else if (operacao === "dividir"){
+		console.log(dividir(a, b))
+	}else{
+		console.log("Função desconhecida")
+	}
 }
 
 // TODO:
 // Faça alguns testes com calcular().
+calcular(10, 5, "somar");
+calcular(10, 5, "subitrair");
+calcular(10, 5, "multiplicar");
+calcular(10, 5, "juntar");
+calcular(10, 0, "dividir");
