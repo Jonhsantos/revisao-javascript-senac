e6a43d13fa9493dfd2659e5d758dd6d16bfec979
Jonhsantos <jonathanssilvasantos1@gmail.com>
2026-08-18 21:10:35 -0300

    feat/arrays-objetos adicionados todos os for e exibido em uma frase formatada

--- 08-catalogo-produtos-arrays-objetos/script.js
+++ 08-catalogo-produtos-arrays-objetos/script.js
@@ -11,10 +11,45 @@
 // preco
 // categoria
 // emEstoque
+const produtos = [
+  { id: 1, nome: "Notebook", preco: 4200, categoria: "Informática", emEstoque: true },
+  { id: 2, nome: "Mouse", preco: 150, categoria: "Acessórios", emEstoque: true },
+  { id: 3, nome: "Monitor", preco: 1600, categoria: "Informática", emEstoque: true },
+  { id: 4, nome: "Teclado", preco: 300, categoria: "Acessórios", emEstoque: true },
+  { id: 5, nome: "Headphone", preco: 350, categoria: "Áudio", emEstoque: false },
+];
 
 // TODO:
 // Percorra o array com for, for...of ou forEach().
+/*
+for (produto in produtos){
+  console.log(produto);
+}
+
+console.log("\n-----\n");
+
+for (const produto of produtos) {
+  console.log(produto);
+}
+
+console.log("\n-----\n");
+
+produtos.forEach(produto => {
+  console.log(produto);
+});
+*/
 
 // TODO:
 // Para cada item, mostre no console uma frase formatada
 // contendo as informações do produto.
+
+produtos.forEach(produto =>{
+  console.log(`
+    ====================================================================
+    Nome do Produto: ${produto.nome}\n\t
+    Preço R$:${produto.preco.toFixed(2)}\n\t
+    Categoria: ${produto.categoria}\n\t
+    Disponibilidade: ${produto.emEstoque == true? "Disponivel": "indisponível"}
+    ====================================================================\n
+    `)
+});
